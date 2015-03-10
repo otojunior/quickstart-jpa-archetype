420192c80cf0c036b1c9af14c89f17effd9b0918
Oto Junior <otojunior@gmail.com>
2015-03-10 16:56:44 -0300

    Deleção de arquivos de demo do arquetipo

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -7,21 +7,14 @@ import org.slf4j.LoggerFactory;
  * Application Main Class.
  * @author [Author name]
  */
-public final class App {
+public class App {
 	/**
 	 * SLF4J Logger.
 	 */
 	private static final Logger LOG = LoggerFactory.getLogger(App.class);
 	
 	/**
-	 * Private Constructor
-	 */
-	private App() {
-		LOG.trace("App instance created");
-	}
-	
-	/**
-	 * Main method.
+	 * Application main method.
 	 * @param args Command line arguments.
 	 */
 	public static void main(String[] args) {
